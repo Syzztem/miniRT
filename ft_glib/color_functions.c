5902dfadc1f370fa565fbf9b26640b7dcf361eec
Louis <louis.thievent@gmail.com>
2019-12-18 13:52:44 +0100

    added light support

--- ft_glib/color_functions.c
+++ ft_glib/color_functions.c
@@ -6,7 +6,7 @@
 /*   By: lothieve <lothieve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/24 18:08:50 by lothieve          #+#    #+#             */
-/*   Updated: 2019/12/15 16:28:00 by lothieve         ###   ########.fr       */
+/*   Updated: 2019/12/18 12:30:20 by lothieve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,16 @@ t_color
 	a.r = sat_add(a.r, b.r);
 	a.g = sat_add(a.g, b.g);
 	a.b = sat_add(a.b, b.b);
+	return (a);
+}
+
+t_color
+	col_multiply_c(t_color a, t_color b)
+{
+	a.r = a.r * b.r / 255;
+	a.g = a.g * b.g / 255;
+	a.b = a.b * b.b / 255;
+
 	return (a);
 }
 
